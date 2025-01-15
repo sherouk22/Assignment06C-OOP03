a7cede29dd657ef1a12755d37ce56989938c31e5
sherouk22 <sheroukmohy558@gmail.com>
2025-01-15 18:33:21 +0200

    Assignment06C#OOP03

---
+++ Assignment06C#OOP03/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment06C_OOP03
+{
+    public class Car
+    {
+        public int? Id { get; set; }
+        public string? Model { get; set; }
+        public decimal Speed { get; set; }
+
+
+        //chain  constructor
+        public Car(int? id , string? model ,decimal speed)
+        {
+            Console.WriteLine("constructor 01");
+            Id = id;
+            Model = model;
+            Speed = speed;
+
+        }
+
+        public Car(int? id, string? model) : this(id, model, 180)
+        {
+            Console.WriteLine("constructor 02");
+        }
+
+        public Car(int? id) : this(id,"BMW") 
+        {
+
+            Console.WriteLine("constructor 03");
+        }
+
+        public override string ToString() 
+        {
+            return $"Id = {Id} \n Model = {Model} \n Speed = {Speed}";
+        }
+
+    }
+}
--- Assignment06C#OOP03/Program.cs
+++ Assignment06C#OOP03/Program.cs
@@ -3,8 +3,29 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {//Assignment06C#OOP03
+
+            //Part 01
+            #region  EX01  all what we have learned in the lecture.
+
+            //Car car = new Car(5, "MBW", 200);
+            //Console.WriteLine(car.ToString());
+
+            //Car car = new Car(5, "MBW");
+            //Console.WriteLine(car.ToString());
+
+            //Car car = new Car(5);
+            //Console.WriteLine(car.ToString());
+
+
+            #endregion
+
+            #region EX02  all what we have learned in the lecture.
+
+
+
+            #endregion
+
         }
     }
 }
